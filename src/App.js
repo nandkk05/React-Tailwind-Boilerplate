95abfbd81ec8ce3f6b451acf82b808bf4c7fda7d
Nand Kishor <nandkk07@gmail.com>
2021-07-06 12:09:18 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GithubCorner from "react-github-corner";
+import logo from "./assets/images/logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="container px-5 mx-auto h-screen justify-center flex flex-wrap flex-col">
+        <img
+          class="xl:w-1/4 lg:w-1/3 md:w-1/2 w-2/3 block mx-auto mb-10 object-cover object-center"
+          alt="logo"
+          src={logo}
+        />
+        <div class="flex flex-col text-center w-full ">
+          <h1 class="text-2xl sm:text-4xl font-bold title-font mb-4 text-gray-900">
+            React X Tailwind CSS
+          </h1>
+          <p class="lg:w-2/3 mx-auto leading-relaxed text-lg">
+            Start building your next project and try to make a dent in the
+            universe.
+          </p>
+          <p class="text-sm mt-8 text-gray-500 mb-8 w-full">
+            Minimal boilerplate, ready-to-use!
+          </p>
+        </div>
+      </div>
+      <GithubCorner
+        href="https://github.com/nandkk05/React-Tailwind-Boilerplate"
+        bannerColor="#00b09b"
+        octoColor="#fff"
+        size={80}
+        direction="right"
+      />
     </div>
   );
 }
